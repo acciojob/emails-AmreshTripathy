6274984d9deb7e017064e60168af0d4fced30ab3
AmreshTripathy <amreshtripathy184@gmail.com>
2023-05-13 12:23:15 +0530

    Email Assignment Push-1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,7 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +18,45 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (!oldPassword.equals(password))
+            return;
+
+        if (newPassword.length() < 8)
+            return;
+
+        char[] arr = newPassword.toCharArray();
+        boolean isUpperCasePresent = false;
+        boolean islowercasePresent = false;
+        boolean isDigitPresent = false;
+        boolean isSpecialCharacterPresent = false;
+
+        int i = 0;
+
+        while (i < arr.length && (!isUpperCasePresent || !islowercasePresent || !isDigitPresent || !isSpecialCharacterPresent)) {
+
+            if (Character.isUpperCase(arr[i]))
+                isUpperCasePresent = true;
+            else if (Character.isLowerCase(arr[i]))
+                islowercasePresent = true;
+            else if (Character.isDigit(arr[i]))
+                isDigitPresent = true;
+            else
+                isSpecialCharacterPresent = true;
+
+            if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
+                break;
+
+            i++;
+        }
+
+        if (isUpperCasePresent && islowercasePresent && isDigitPresent && isSpecialCharacterPresent)
+            password = newPassword;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -6,25 +6,56 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Workspace extends Gmail{
+public class Workspace extends Gmail {
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId, Integer.MAX_VALUE);
+        calendar = new ArrayList<>();
     }
 
-    public void addMeeting(Meeting meeting){
+    public void addMeeting(Meeting meeting) {
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
+    public int findMaxMeetings() {
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        calendar.sort((a, b) -> {
+            if (a.endTime.equals(b.endTime)) {
+                if (a.startTime.isBefore(b.startTime))
+                    return -1;
+                else if (a.startTime.isAfter(b.startTime))
+                    return 1;
+                else
+                    return 0;
+            } else {
+                if (a.endTime.isBefore(b.endTime))
+                    return -1;
+                else if (a.endTime.isAfter(b.endTime))
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+
+        LocalTime curEnd = calendar.get(0).endTime;
+
+        int count = 0;
+
+        for (int i = 1; i < calendar.size(); i++) {
+            if (calendar.get(i).endTime.isAfter(curEnd)) {
+                count++;
+                curEnd = calendar.get(i).endTime;
+            }
+        }
+
+        return count;
     }
 }
